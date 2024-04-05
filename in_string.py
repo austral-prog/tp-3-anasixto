2b2f5b260b150d3fafcca3f1f8ce4e272f325481
anasixto <162721927+anasixto@users.noreply.github.com>
2024-04-05 12:45:11 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -5,11 +5,11 @@ def check_vowels():
     my_name = input()
 
     # Imprime en pantalla
-    print(("Contiene a:") + str("a" in My_name. lower())
-    print(("Contiene e:") + str("e" in My_name. lower())
-    print(("Contiene i:") + str("i" in My_name. lower())
-    print(("Contiene o:") + str("o" in My_name. lower())
-    print(("Contiene u:") + str("u" in My_name. lower())
+    print(("Contiene a: " + str("a" in My_name.lower())
+    print(("Contiene e: " + str("e" in My_name.lower())
+    print(("Contiene i: " + str("i" in My_name.lower())
+    print(("Contiene o: " + str("o" in My_name.lower())
+    print(("Contiene u: " + str("u" in My_name.lower())
     
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
