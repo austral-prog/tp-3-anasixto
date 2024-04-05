135e7ed518cb2ebd1da41177f75b0bc88ceb278d
anasixto <162721927+anasixto@users.noreply.github.com>
2024-04-05 12:06:38 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,7 +1,11 @@
 def slice_simple():
-    texto = "Awesome"
+    text = "Awesome"
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+    # Imprime en pantalla
+    print(text. lower()[0:3])
+    print(text[2:5])
+    print(text. lower()[0:7])
 
 
 # Para verificar este ejercicio ejecutar el comando
